b65f633e19d0f31323443f2f0c136b11f9a1da2b
Justin Menga <justin.menga@gmail.com>
2016-10-09 17:44:55 +1300

    Add unit tests

---
+++ src/test/java/com/pluralsight/dockerproductionaws/portfolio/PortfolioServiceSellTest.java
@@ -0,0 +1,84 @@
+package com.pluralsight.dockerproductionaws.portfolio;
+
+import com.pluralsight.dockerproductionaws.portfolio.impl.PortfolioServiceImpl;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import io.vertx.servicediscovery.ServiceDiscovery;
+import io.vertx.servicediscovery.ServiceDiscoveryOptions;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by jmenga on 9/10/16.
+ */
+@RunWith(VertxUnitRunner.class)
+public class PortfolioServiceSellTest {
+    private Config config = ConfigFactory.load();
+    private Portfolio portfolio;
+    private Vertx vertx;
+    private ServiceDiscovery discovery;
+    private PortfolioService svc;
+    private JsonObject getQuote() {
+        return new JsonObject()
+                .put("exchange", "vert.x stock exchange")
+                .put("symbol", "MCH")
+                .put("name", "MacroHard")
+                .put("bid", 3328.0)
+                .put("ask", 3329.0)
+                .put("volume", 3)
+                .put("open", 600.0)
+                .put("shares", 3);
+
+    }
+
+    @Before
+    public void testSetup(TestContext context) {
+        vertx = Vertx.vertx();
+        discovery = ServiceDiscovery
+                .create(vertx, new ServiceDiscoveryOptions()
+                        .setBackendConfiguration(vertx.getOrCreateContext().config()));
+        svc = new PortfolioServiceImpl(vertx, discovery, 10000);
+        svc.getPortfolio(ar -> {
+            portfolio = ar.result();
+            svc.buy(3, getQuote(), result -> {
+                context.async().complete();
+            });
+        });
+    }
+
+    @Test
+    public void testSell(TestContext context) {
+        svc.sell(2, getQuote(), result -> {
+            assertThat(result.succeeded()).isTrue();
+            assertThat(portfolio.getCash()).isEqualTo(6669);
+            assertThat(portfolio.getShares().get("MacroHard")).isEqualTo(1);
+            context.async().complete();
+        });
+    }
+
+    @Test
+    public void testSellNotEnoughStocks(TestContext context) {
+        svc.sell(4, getQuote(), result -> {
+            assertThat(result.succeeded()).isFalse();
+            assertThat(portfolio.getCash()).isEqualTo(13);
+            context.async().complete();
+        });
+    }
+
+    @Test
+    public void testSellNegativeStocks(TestContext context) {
+        svc.sell(-1, getQuote(), result -> {
+            assertThat(result.succeeded()).isFalse();
+            assertThat(portfolio.getCash()).isEqualTo(13);
+            context.async().complete();
+        });
+    }
+}
